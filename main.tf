944942a3a946a02f66472e4bc75bc2939c11c6fa
chetunaik <113455610+chetunaik@users.noreply.github.com>
2024-09-19 18:26:03 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,119 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "myvpc"
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.myvpc.id
+  tags = {
+    Name = "myigw"
+  }
+}
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.myvpc.id
+  tags = {
+     Name = "public-rt"
+  }
+}
+
+resource "aws_route" "public-route" {
+    route_table_id = aws_route_table.public-rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myigw.id
+}
+
+resource "aws_route_table_association" "public-subnet-association" {
+  subnet_id = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+resource "aws_security_group" "websg" {
+  vpc_id = aws_vpc.myvpc.id
+  name = "websg"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = "0.0.0.0/0"
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = "0.0.0.0/0"
+  }
+
+  tags = {
+    Name = "websg"
+  }
+}
+  
+resource "aws_security_group" "dbsg" {
+  vpc_id = aws_vpc.myvpc.id
+  name = "dbsg"
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = "10.0.1.0/24"
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = "0.0.0.0/0"
+  }
+
+  tags = {
+    Name = "dbsg"
+  }
+}
+
+resource "aws_instance" "webapp" {
+  ami = "ami-0ebfd941bbafe70c6"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  subnet_id = aws_subnet.public-subnet.id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "webapp"
+  }
+}
+
+resource "aws_instance" "dbapp" {
+  ami = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.dbsg.id]
+  subnet_id = aws_subnet.private-subnet.id
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "dbapp"
+  }
+}
