2824ee620d75693b609a22a5186526d188eb79cd
chetunaik <113455610+chetunaik@users.noreply.github.com>
2024-09-19 19:00:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,14 +56,14 @@ resource "aws_security_group" "websg" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -79,14 +79,14 @@ resource "aws_security_group" "dbsg" {
     from_port = 3306
     to_port = 3306
     protocol = "tcp"
-    cidr_blocks = "10.0.1.0/24"
+    cidr_blocks = ["10.0.1.0/24"]
   }
 
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
